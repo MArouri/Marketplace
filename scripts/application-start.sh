7ec18eb2086b8efb7d0cba272f8fe5733be2edfb
Arouri <mohammadarouri@gmail.com>
2020-03-07 20:22:34 +0200

    Update build spec and application start

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -3,5 +3,5 @@ cd /tmp/CodeDeployExample
 
 echo "The ApplicationStart deployment lifecycle event successfully completed." > application-start.txt
 
-mv marketplace-0.0.1-SNAPSHOT.war ROOT.war
+mv build/libs/marketplace-0.0.1-SNAPSHOT.war ROOT.war
 java -jar ROOT.war &
