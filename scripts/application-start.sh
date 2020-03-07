312a10ceff5e99cd82574ba60eca2028f13f6a02
Arouri <mohammadarouri@gmail.com>
2020-03-07 20:17:27 +0200

    Add app spec

---
+++ scripts/application-start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /tmp/CodeDeployExample
+
+echo "The ApplicationStart deployment lifecycle event successfully completed." > application-start.txt
+
+mv marketplace-0.0.1-SNAPSHOT.war ROOT.war
+java -jar ROOT.war &
