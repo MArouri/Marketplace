fafd9df85e7e5529fa06d64fc9351ff67d07b9c2
Arouri <mohammadarouri@gmail.com>
2020-03-21 14:15:03 +0200

    Temp stop tomcat

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -4,4 +4,4 @@ cd /tmp/CodeDeployExample
 echo "The ApplicationStart deployment lifecycle event successfully completed." > application-start.txt
 
 mv marketplace-0.0.1-SNAPSHOT.war ROOT.war
-java -jar ROOT.war >/dev/null 2>&1
+#java -jar ROOT.war >/dev/null 2>&1
