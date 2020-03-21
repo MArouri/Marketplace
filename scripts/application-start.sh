c88b6837a3ccbd0a511b1decdabfa86e672d83d2
Arouri <mohammadarouri@gmail.com>
2020-03-21 14:34:57 +0200

    stop tomcat

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -3,5 +3,5 @@ cd /tmp/CodeDeployExample
 
 echo "The ApplicationStart deployment lifecycle event successfully completed." > application-start.txt
 
-mv marketplace-0.0.1-SNAPSHOT.war ROOT.war
-java -jar ROOT.war > /dev/null 2>&1 &
+#mv marketplace-0.0.1-SNAPSHOT.war ROOT.war
+#java -jar ROOT.war > /dev/null 2>&1 &
