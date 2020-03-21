15292281142b7364df1ca59f206bbbeeac380caf
Arouri <mohammadarouri@gmail.com>
2020-03-21 17:54:15 +0200

    Update application-start.sh

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -4,4 +4,4 @@ cd /tmp/CodeDeployExample
 echo "The ApplicationStart deployment lifecycle event successfully completed." > application-start.txt
 
 mv marketplace-0.0.1-SNAPSHOT.war ROOT.war
-java -jar ROOT.war > logs.txt &
+java -jar ROOT.war > logs.txt 2>&1 &
